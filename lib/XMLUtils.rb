ccf3ed53733814ba5070c2fcdf95b633751adea2
Pedro Côrte-Real <pedro@pedrocr.net>
2006-06-07 11:05:28 +0000

    Implement a XMLUtils::get_xpath to fetch the contents of an element from a document.

--- lib/XMLUtils.rb
+++ lib/XMLUtils.rb
@@ -1,4 +1,5 @@
-require "rexml/document"
+require 'rexml/document'
+require 'stringio'
 
 # This module holds generic XML utilities. The module's methods are simple 
 # XML utilities. The module also contains XMLStreamParser, a Generic XML Stream 
@@ -54,5 +55,52 @@ module XMLUtils
     str = ''
     t.write_with_substitution(str, string)
     str
+  end
+  
+  # Gets the xpath inside a given document that can either be a string or a 
+  # REXML::Document
+  #
+  # opts can have:
+  #   :multiple: fetch all the occurences of the xpath
+  #   :with_attrs: include the attribute contents in the result
+  #   :recursive: recursively include all the subelements of the matches
+  def self.get_xpath(path, doc, opts={})
+    if doc.is_a? REXML::Document
+      doc = doc
+    else
+      doc = REXML::Document.new(doc)
+    end
+    
+    if opts[:multiple]
+      strs = []
+      REXML::XPath.each(doc.root, path) do |e|
+        t = get_rexml_content(e, opts)
+        strs << t if t != ''
+      end
+      return strs.join(' ')
+    else
+      return get_rexml_content(REXML::XPath.first(doc, path), opts)
+    end
+  end
+  
+  def self.get_rexml_content(element, opts={})
+    return '' if not element
+    strs = []
+    if element.respond_to?('value')
+      return element.value || ''
+    else
+      if opts[:with_attrs]
+        element.attributes.each {|name, value| strs << value if value != ''}
+      end
+      if not opts[:recursive]
+        element.texts.each {|t| strs << t if t != ''}
+      else  
+        element.to_a.each do |e|
+          t = get_rexml_content(e, opts)
+          strs << t if t != ''
+        end
+      end
+    end
+    strs.join(' ')
   end
 end
--- test/TestXMLUtils.rb
+++ test/TestXMLUtils.rb
@@ -4,14 +4,47 @@ require 'test/unit'
 require 'XMLUtils'
 
 class TestXMLUtils < Test::Unit::TestCase
-	def test_create_open_tag
-		tag = XMLUtils::create_open_tag("name", {"arg1" => "val1", "arg2" => "val2"})
-		assert_equal("<name arg1='val1' arg2='val2'>", tag)	
-	end
-	
-	def test_escape_xml
-	  assert_equal '&lt; abc', XMLUtils::escape_xml('< abc')
-	  assert_equal '&gt; abc', XMLUtils::escape_xml('> abc')
-	  assert_equal '&amp; abc', XMLUtils::escape_xml('& abc')
-	end
+  def test_create_open_tag
+    tag = XMLUtils::create_open_tag("name", {"arg1" => "val1", "arg2" => "val2"})
+    assert_equal("<name arg1='val1' arg2='val2'>", tag)  
+  end
+  
+  def test_escape_xml
+    assert_equal '&lt; abc', XMLUtils::escape_xml('< abc')
+    assert_equal '&gt; abc', XMLUtils::escape_xml('> abc')
+    assert_equal '&amp; abc', XMLUtils::escape_xml('& abc')
+  end
+  
+  def test_get_xpath
+    opts = {}
+    assert_xpath_equal('abc', '//xpto', '<xpto>abc<xpto>')
+    
+    assert_xpath_equal('abc', '//xpto', '<xpto>abc<xpto2>foo</xpto2><xpto>')
+    
+    opts[:with_attrs] = true
+    opts[:recursive] = false
+    opts[:multiple] = false
+    assert_xpath_equal('attr1 abc', '//xpto', '<xpto attr1="attr1">abc<xpto2>foo</xpto2><xpto>', opts)
+    
+    opts[:with_attrs] = false
+    opts[:recursive] = true
+    opts[:multiple] = false
+    assert_xpath_equal('abc foo', '//xpto', '<xpto>abc<xpto2 attr1="attr1">foo</xpto2><xpto>', opts)
+    
+    opts[:with_attrs] = true
+    opts[:recursive] = true
+    opts[:multiple] = false
+    assert_xpath_equal('abc attr1 foo', '//xpto', '<xpto>abc<xpto2 attr1="attr1">foo</xpto2><xpto>', opts)
+    
+    opts[:with_attrs] = false
+    opts[:recursive] = false
+    opts[:multiple] = true
+    assert_xpath_equal('abc foo', '//xpto', '<xpto>abc<xpto attr1="attr1">foo</xpto><xpto>', opts)
+  end
+  
+  def assert_xpath_equal(result, path, xml, opts={})
+    assert_equal result, XMLUtils::get_xpath(path, xml, opts)
+    assert_equal result, XMLUtils::get_xpath(path, StringIO.new(xml), opts)
+    assert_equal result, XMLUtils::get_xpath(path, REXML::Document.new(xml), opts)
+  end
 end
