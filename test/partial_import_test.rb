e788376f42e460bbcca75639827c2fc0c1b435d4
Pedro Côrte-Real <pedro@pedrocr.net>
2013-08-30 14:35:58 +0100

    DRY in tests for DOM/Text

--- test/partial_import_test.rb
+++ test/partial_import_test.rb
@@ -12,27 +12,30 @@ class TestPartialImport < Test::Unit::TestCase
     xmlattr :myattr
   end
 
-  def test_partial_elements_in_text
-    sel = PartialBaseFormat.import_xml "<abc><otherel></otherel>text</abc>"
-    assert_equal "text", sel.value
+  # Tests both through DOM and text to make sure both code paths are working
+  def double_test(text)
+    sel = PartialBaseFormat.import_xml text
+    yield sel
+    sel = PartialBaseFormat.import_xml Nokogiri::XML::Document.parse(text)
+    yield sel
   end
 
-  def test_partial_elements_in_dom
-    xml_text = "<abc><otherel></otherel>text</abc>"
-    sel = PartialBaseFormat.import_xml Nokogiri::XML::Document.parse(xml_text)
-    assert_equal "text", sel.value
+  def test_partial_elements
+    double_test("<abc><otherel></otherel>text</abc>") do |sel|
+      assert_equal "text", sel.value
+    end
   end
 
   def test_partial_attributes_in_text
-    sel = PartialBaseFormat.import_xml "<abc myattr='real' someattr='xpto'>text</abc>"
-    assert_equal "text", sel.value
-    assert_equal "real", sel.myattr
+    double_test("<abc myattr='real' someattr='xpto'>text</abc>") do |sel|
+      assert_equal "text", sel.value
+      assert_equal "real", sel.myattr
+    end
   end
 
-  def test_partial_attributes_in_dom
-    xml_text = "<abc myattr='real' someattr='xpto'>text</abc>"
-    sel = PartialBaseFormat.import_xml Nokogiri::XML::Document.parse(xml_text)
-    assert_equal "text", sel.value
-    assert_equal "real", sel.myattr
+  def test_stress_valid_subelements_in_text
+    double_test("<abc><otherel><abc></abc></otherel>text</abc>") do |sel|
+      assert_equal "<abc>text</abc>", sel.xml_text
+    end
   end
 end
